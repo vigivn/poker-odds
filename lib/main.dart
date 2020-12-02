41bb13053c236d7ee552eab042a27a2dc98ddead
Vigiv <42895139+Vigiv@users.noreply.github.com>
2020-12-02 18:10:14 +0300

    added only portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:poker_odds/data/available_cards_data.dart';
 import 'package:poker_odds/data/card_fields_data.dart';
 import 'package:provider/provider.dart';
@@ -9,21 +10,27 @@ main(List<String> args) => runApp(PokerChanceApp());
 class PokerChanceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<CardFieldsData>(
-      create: (context) => CardFieldsData(),
-      child: ChangeNotifierProvider<AvailableCardsData>(
-        create: (context) => AvailableCardsData(),
-        child: MaterialApp(
-          title: "Poker Odds",
-          theme: ThemeData(
-            primaryColor: Colors.indigo,
-            accentColor: Colors.indigoAccent,
-            textTheme: TextTheme(
-              subtitle1: TextStyle(color: Colors.white),
-            ),
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CardFieldsData>(
+            create: (context) => CardFieldsData()),
+        ChangeNotifierProvider<AvailableCardsData>(
+            create: (context) => AvailableCardsData()),
+      ],
+      child: MaterialApp(
+        title: "Poker Odds",
+        theme: ThemeData(
+          primaryColor: Colors.indigo,
+          accentColor: Colors.indigoAccent,
+          textTheme: TextTheme(
+            subtitle1: TextStyle(color: Colors.white),
           ),
-          home: HomePage(),
         ),
+        home: HomePage(),
       ),
     );
   }
